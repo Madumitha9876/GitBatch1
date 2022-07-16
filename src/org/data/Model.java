47e003a308be1c61ba4a6ce1af44d610af69bc67
Madumitha9876 <cmadumi89@gmail.com>
2022-07-16 22:06:28 +0530

    Branch two coding

--- src/org/data/Model.java
+++ src/org/data/Model.java
@@ -1,6 +1,10 @@
 package org.data;
 
 public class Model {
+	private void m3() {
+		System.out.println("Branch2 code");
+
+	}
 public static void main(String[] args) {
 	System.out.println("Im a master");
 }
